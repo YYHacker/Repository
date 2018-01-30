b20daa805dcae8ab4a1818d841effb3a98429b7c
YYHacker <993031727@qq.com>
2018-01-30 14:33:31 +0800

    This is frist commit

---
+++ AvDemo.java
@@ -0,0 +1,45 @@
+package com.client.ebulid;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import com.travelsky.ibe.client.AV;
+import com.travelsky.ibe.client.AvItem;
+import com.travelsky.ibe.client.AvResult;
+import com.travelsky.ibe.client.pnr.DETR;
+import com.travelsky.ibe.client.pnr.DETRTKTResult;
+
+public class AvDemo {
+	public static void main(String[] args) {
+		AV av = new AV();
+		av.setConnectionInfo("10.221.136.60", 6891);//设置ip端口
+		av.setAgentInfo("SCH115", "0", "20");//设置代理人信息
+		av.setAppName("3uairetn1");//设置服务名称
+		
+		Calendar orgdate = Calendar.getInstance();
+		orgdate.add(Calendar.DATE, 1);
+		orgdate.set(Calendar.HOUR_OF_DAY,0);
+		orgdate.set(Calendar.MINUTE,0);
+		orgdate.set(Calendar.SECOND,0);
+		try {
+			AvResult result = av.getAvailability("CTU","SEA",orgdate.getTime());
+			result.sortItems('p');
+			System.out.println(result);
+			for (int i = 0; i < result.getItemamount(); i++) {
+				AvItem avItem = result.getItem(i);
+				System.out.println("=================="+avItem.getSegmentnumber()+"====================");
+				for (int j = 0; j < avItem.getSegmentnumber(); j++) {
+					int hour = Integer.valueOf(avItem.getDeptime(j).substring(0, 2));
+					System.out.println("查询信息如下：");
+					System.out.print("出发城市："+avItem.getOrgcity(j)+"/"+"到达城市："+avItem.getDstcity(j));
+					System.out.print("出发时间："+avItem.getDepdate(j)+"/"+avItem.getDeptime(j));
+					System.out.print("航班号："+avItem.getAirline(j));
+				}
+			}
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}	
---
+++ EBTest2.java
@@ -0,0 +1,51 @@
+package com.client.ebulid;
+
+import java.util.Calendar;
+
+import com.travelsky.ibe.client.*;
+
+public class EBTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AV av = new AV();
+		av.setConnectionInfo("10.221.136.60", 6891);
+		av.setAgentInfo("SCH115", "0", "20");
+		av.setAppName("3uairetn1");
+		try {
+			Calendar cal = Calendar.getInstance();
+			cal.add(Calendar.DATE, 1);
+			cal.set(Calendar.HOUR_OF_DAY, 0);
+			cal.set(Calendar.MINUTE, 0);
+			cal.set(Calendar.SECOND, 0);
+			Calendar sCal = Calendar.getInstance();
+			sCal.add(Calendar.DATE, 1);
+			sCal.set(Calendar.HOUR_OF_DAY, 11);
+			sCal.set(Calendar.MINUTE, 59);
+			sCal.set(Calendar.SECOND, 59);
+			Calendar eCal = Calendar.getInstance();
+			eCal.add(Calendar.DATE, 1);
+			eCal.set(Calendar.HOUR_OF_DAY,12);
+			eCal.set(Calendar.MINUTE, 59);
+			eCal.set(Calendar.SECOND, 59);
+			AvResult avResult = av.getAvailability("CTU","SEA", cal.getTime());
+			System.out.println(avResult.getItemamount());
+			for(int i = 0 ;i < avResult.getItemamount();i++){
+				AvItem item = avResult.getItem(i);
+				System.out.println(item.getSegmentnumber());
+				for (int j = 0; j < item.getSegmentnumber(); j++) {
+					if(!(item.getDepdate(j).getTime() >= sCal.getTime().getTime() && item.getDepdate(j).getTime() <= eCal.getTime().getTime())){
+						System.out.print("出发:"+item.getOrgcity(j) + ",");
+						System.out.print("到达:"+item.getDstcity(j) + ",");
+						System.out.print("出发时间:"+item.getDepdate(j) + ",");
+						System.out.println("航班号:"+item.getAirline(j) + ";");
+					}
+				}
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
